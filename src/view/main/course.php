60223725f5e750e654524c357ccb21faab1cc482
Jose <jmg24a@gmail.com>
2023-07-01 17:43:48 -0400

    pagina cursos

--- src/view/main/course.php
+++ src/view/main/course.php
@@ -4,35 +4,47 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel='stylesheet' href="<?php echo constant('URL');?>public/css/main/main.css">
+  <link rel='stylesheet' href="<?php echo constant('URL');?>public/css/main/courses.css">
   <title>Document</title>
 </head>
 <body>
-  <? require 'view/components/layout/header.php'; ?>
-  <section class="paralax" style="background: rgba(200, 11, 11, 0.851);">
-    <h2 class="paralax_title" id="text">
-      INCES
-    </h2>
-    <!-- <img src="<? echo constant('URL');?>public/img/hill1.png" id="hill1" alt="hill1">
-    <img src="<? echo constant('URL');?>public/img/hill2.png" id="hill2" alt="hill2">
-    <img src="<? echo constant('URL');?>public/img/hill3.png" id="hill3" alt="hill3">
-    <img src="<? echo constant('URL');?>public/img/hill4.png" id="hill4" alt="hill4">
-    <img src="<? echo constant('URL');?>public/img/hill5.png" id="hill5" alt="hill5">
-    <img src="<? echo constant('URL');?>public/img/leaf.png" id="leaf" alt="leaf">
-    <img src="<? echo constant('URL');?>public/img/plant.png" id="plant" alt="plant">
-    <img src="<? echo constant('URL');?>public/img/tree.png" id="tree" alt="tree"> -->
+  <?php require 'view/components/layout/header.php'; ?>
+  <section class="content" style="background: rgba(200, 11, 11, 0.851);">
+    <div class="content_courses">
+      <div class="card_course">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexaUrvepPx2w3g_n9htsR3t3Lp5wX0M1P_myEZCHxHJEm67DBnizWYYPV9Ig1tsjNjRM&usqp=CAU" alt="electricista">
+        <h4>Electricidad</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis sit reiciendis. Exercitationem eligendi, facere harum illum alias nobis eius velit, ab pariatur minima ducimus cum praesentium, aspernatur officiis unde.
+        </p>
+        <button>Saber mas</button>
+      </div>
+      <div class="card_course">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexaUrvepPx2w3g_n9htsR3t3Lp5wX0M1P_myEZCHxHJEm67DBnizWYYPV9Ig1tsjNjRM&usqp=CAU" alt="electricista">
+        <h4>Electricidad</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis sit reiciendis. Exercitationem eligendi, facere harum illum alias nobis eius velit, ab pariatur minima ducimus cum praesentium, aspernatur officiis unde.
+        </p>
+        <button>Saber mas</button>
+      </div>
+      <div class="card_course">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexaUrvepPx2w3g_n9htsR3t3Lp5wX0M1P_myEZCHxHJEm67DBnizWYYPV9Ig1tsjNjRM&usqp=CAU" alt="electricista">
+        <h4>Electricidad</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis sit reiciendis. Exercitationem eligendi, facere harum illum alias nobis eius velit, ab pariatur minima ducimus cum praesentium, aspernatur officiis unde.
+        </p>
+        <button>Saber mas</button>
+      </div>
+      <div class="card_course">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRexaUrvepPx2w3g_n9htsR3t3Lp5wX0M1P_myEZCHxHJEm67DBnizWYYPV9Ig1tsjNjRM&usqp=CAU" alt="electricista">
+        <h4>Electricidad</h4>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis sit reiciendis. Exercitationem eligendi, facere harum illum alias nobis eius velit, ab pariatur minima ducimus cum praesentium, aspernatur officiis unde.
+        </p>
+        <button>Saber mas</button>
+      </div>
+    </div>
   </section>
-  <section class="content">
-    <h3 class="main">Que es el INCES</h3>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?    Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nobis tempora in dolore. Modi nulla molestias, illo beatae saepe explicabo aspernatur? Aut labore inventore atque odit aspernatur similique ipsam provident?
-  </section>
-
   <?php require 'view/components/layout/footer.php'; ?>
   <script src="<?php echo constant('URL');?>public/js/paralaxs.js"></script>
 
